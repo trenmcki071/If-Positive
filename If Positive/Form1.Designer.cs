4dc3a2a8d9cb0069d64c21c25861c2e045a7ff8c
trenmcki071 <trenmcki071@outlook.com>
2021-05-06 10:18:14 -0400

    Add project files.

---
+++ If Positive/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace If_Positive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.integerLabel = new System.Windows.Forms.Label();
+            this.inputNumberBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.positiveLabel = new System.Windows.Forms.Label();
+            this.divisibleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // integerLabel
+            // 
+            this.integerLabel.Location = new System.Drawing.Point(37, 36);
+            this.integerLabel.Name = "integerLabel";
+            this.integerLabel.Size = new System.Drawing.Size(100, 27);
+            this.integerLabel.TabIndex = 0;
+            this.integerLabel.Text = "Input an integer";
+            // 
+            // inputNumberBox
+            // 
+            this.inputNumberBox.Location = new System.Drawing.Point(156, 33);
+            this.inputNumberBox.Name = "inputNumberBox";
+            this.inputNumberBox.Size = new System.Drawing.Size(66, 20);
+            this.inputNumberBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(156, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // positiveLabel
+            // 
+            this.positiveLabel.AutoSize = true;
+            this.positiveLabel.Location = new System.Drawing.Point(37, 137);
+            this.positiveLabel.Name = "positiveLabel";
+            this.positiveLabel.Size = new System.Drawing.Size(121, 13);
+            this.positiveLabel.TabIndex = 3;
+            this.positiveLabel.Text = "__ is an ______ number";
+            // 
+            // divisibleLabel
+            // 
+            this.divisibleLabel.AutoSize = true;
+            this.divisibleLabel.Location = new System.Drawing.Point(37, 184);
+            this.divisibleLabel.Name = "divisibleLabel";
+            this.divisibleLabel.Size = new System.Drawing.Size(91, 13);
+            this.divisibleLabel.TabIndex = 4;
+            this.divisibleLabel.Text = "__ is ______ by 7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 229);
+            this.Controls.Add(this.divisibleLabel);
+            this.Controls.Add(this.positiveLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.inputNumberBox);
+            this.Controls.Add(this.integerLabel);
+            this.Name = "Form1";
+            this.Text = "Numbers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label integerLabel;
+        private System.Windows.Forms.TextBox inputNumberBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label positiveLabel;
+        private System.Windows.Forms.Label divisibleLabel;
+    }
+}
+
---
+++ If Positive/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace If_Positive
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int inputNumber;
+        double remainder;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                inputNumber = Int32.Parse(inputNumberBox.Text);
+                if (inputNumber > 0)
+                {
+                    positiveLabel.Text = $"{inputNumber} is a positive number";
+                }
+                else
+                {
+                    positiveLabel.Text = $"{inputNumber} is not a positive number";
+                }
+                remainder = inputNumber % 7;
+                if (remainder == 0)
+                {
+                    divisibleLabel.Text = $"{inputNumber} is divisible by 7";
+                }
+                else
+                {
+                    divisibleLabel.Text = $"{inputNumber} is not divisible by 7";
+                }
+            }
+            catch { 
+                integerLabel.Text = "I said an integer";
+                positiveLabel.Text = "___ is a ________ number";
+                  }
+        }
+    }
+}
